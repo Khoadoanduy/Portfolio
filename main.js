e0b9a68d7ff30b2bac82982acf32513151189609
Khoadoanduy <khoadoanduy05@gmail.com>
2023-12-27 09:11:42 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function openPDF() {
+    // Replace 'path/to/your/pdf/file.pdf' with the actual path to your PDF file
+    var pdfUrl = 'Kevin_Resume.pdf';
+    
+    // Open the PDF in a new tab
+    window.open(pdfUrl, '_blank');
+  }
