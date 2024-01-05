6ac0fbf3352de877f897de260c400d8cab85ff25
khoadoanduy <khoadoanduy05@gmail.com>
2024-01-05 16:20:14 -0600

    add scroll animation, fix ul list type, fix a href navigation

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function myFunction() {
         x.className = "navbar";
     }
 }
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top; // Fix typo here
+        var revealpoint = 150;
+        if (revealTop < windowheight - revealpoint) { // Fix typo here
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    }
+}
