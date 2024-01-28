1d03de3c33eb0cb359fa4ba6804e30183a5be027
khoadoanduy <khoadoanduy05@gmail.com>
2024-01-28 14:13:09 -0600

    Change resume file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function openPDF() {
     // Replace 'path/to/your/pdf/file.pdf' with the actual path to your PDF file
-    var pdfUrl = 'Kevin_Resume.pdf';
+    var pdfUrl = 'Duy_Khoa_Doan_resume.pdf';
     
     // Open the PDF in a new tab
     window.open(pdfUrl, '_blank');
